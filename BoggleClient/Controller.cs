6f29dd11a7b6a5c1e9000d99bdfe8e5905a81799
Samuel Zachary <sazzach@gmail.com>
2016-03-22 16:34:24 -0600

    Added method headers.

--- BoggleClient/Controller.cs
+++ BoggleClient/Controller.cs
@@ -8,5 +8,37 @@ namespace BoggleClient
 {
     class Controller
     {
+        public Controller()
+        {
+
+        }
+
+        /// <summary>
+        /// Registers the player with the boggle server.
+        /// Sets the id of the player in the model.
+        /// </summary>
+        /// <param name="nickName">Desired name of the player.</param>
+        private void registerPlayer(string nickName)
+        {
+
+        }
+
+        /// <summary>
+        /// Joins a game or starts a new game.
+        /// 
+        /// State should go to waiting for game to start.
+        /// </summary>
+        private void joinGame()
+        {
+
+        }
+
+        /// <summary>
+        /// Cancel a pending request to join a game.
+        /// </summary>
+        private void cancelJoinRequest()
+        {
+
+        }
     }
 }
