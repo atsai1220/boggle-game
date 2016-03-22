68428581d4991500773cc2801bb3063f201d79ee
Andrew Tsai <andrewtsai1220@hotmail.com>
2016-03-22 13:44:05 -0600

    Initial setup BogleGUI, IBoggleAPI, Model, Controller

---
+++ BoggleClient/BoggleGUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoggleClient
+{
+    public partial class BoggleGUI : Form
+    {
+        public BoggleGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void BoggleGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
