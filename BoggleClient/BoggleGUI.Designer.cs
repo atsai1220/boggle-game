0eba3a1296bb98378e1332a67fe748d752f6ed23
Andrew Tsai <andrewtsai1220@hotmail.com>
2016-03-23 22:32:01 -0600

    Added GUI for words and timer

--- BoggleClient/BoggleGUI.Designer.cs
+++ BoggleClient/BoggleGUI.Designer.cs
@@ -57,6 +57,14 @@
             this.box2 = new System.Windows.Forms.Button();
             this.box1 = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.wordBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.gamePanel.SuspendLayout();
             this.cubeLayoutPanel.SuspendLayout();
@@ -133,6 +141,14 @@
             // 
             // gamePanel
             // 
+            this.gamePanel.Controls.Add(this.textBox3);
+            this.gamePanel.Controls.Add(this.label4);
+            this.gamePanel.Controls.Add(this.wordBox);
+            this.gamePanel.Controls.Add(this.textBox2);
+            this.gamePanel.Controls.Add(this.textBox1);
+            this.gamePanel.Controls.Add(this.label3);
+            this.gamePanel.Controls.Add(this.label2);
+            this.gamePanel.Controls.Add(this.label1);
             this.gamePanel.Controls.Add(this.cubeLayoutPanel);
             this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gamePanel.Location = new System.Drawing.Point(0, 0);
@@ -340,6 +356,79 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(420, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "My Words";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Words:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(462, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(462, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // wordBox
+            // 
+            this.wordBox.Cursor = System.Windows.Forms.Cursors.No;
+            this.wordBox.DetectUrls = false;
+            this.wordBox.Location = new System.Drawing.Point(424, 103);
+            this.wordBox.Name = "wordBox";
+            this.wordBox.ReadOnly = true;
+            this.wordBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.wordBox.Size = new System.Drawing.Size(139, 227);
+            this.wordBox.TabIndex = 6;
+            this.wordBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(424, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Time remaining (sec):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 354);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 8;
+            // 
             // BoggleGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -355,6 +444,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.gamePanel.ResumeLayout(false);
+            this.gamePanel.PerformLayout();
             this.cubeLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -391,6 +481,14 @@
         private System.Windows.Forms.Button box3;
         private System.Windows.Forms.Button box2;
         private System.Windows.Forms.Button box1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox wordBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
