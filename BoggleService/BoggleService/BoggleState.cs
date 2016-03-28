a455225599681568248853e0ab53fad349ca1abd
Andrew Tsai <andrewtsai1220@hotmail.com>
2016-03-28 16:53:08 -0600

    Added IBoggleState.cs and BoggleState.cs commented BoggleState.cs

---
+++ BoggleService/BoggleService/BoggleState.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle
+{
+    class BoggleState : IBoggleState
+    {
+        // userToken -> nickName
+        Dictionary<string, string> players;
+        Dictionary<string, BoggleGame> gameId;
+
+        struct BoggleGame
+        {
+            string gameId;
+            string board;
+            int timeLimit;
+            long startTime;
+
+            string player1UserToken;
+            int player1Score;
+            List<string> player1Words;
+
+            string player2UserToken;
+            int player2Score;
+            List<string> player2Words;
+        }
+
+        //s
+        public void AddGame(string gameId, string player1Token, int player1TimeLimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public void AddWord(string gameId, string userToken, string word, int score)
+        {
+            throw new NotImplementedException();
+        }
+
+        //s
+        public void CancelGame(string gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public void CreateUser(string nickname, string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        //s
+        public string GetBoard(string gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public string GetNickname(string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        //s
+        public void GetPlayers(string gameId, out string player1Id, out string player2Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public int GetScore(string gameId, string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        //s
+        public void GetTime(string gameId, out int timeLimit, out long startTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public List<string> GetWords(string gameId, string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        //s
+        public void SetScore(string gameId, string userToken, int score)
+        {
+            throw new NotImplementedException();
+        }
+
+        //a
+        public void StartGame(string gameId, string player2Token, int player2TimeLimit, long startTime, string board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BoggleService/BoggleService/IBoggleState.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle
+{
+    interface IBoggleState
+    {
+
+        void CreateUser(string nickname, string userToken);
+
+        /// <summary>
+        /// Returns the nickname for the given userToken.
+        /// 
+        /// If the userToken does not exist returns an empty string.
+        /// </summary>
+        /// <param name="userToken">The user token</param>
+        /// <returns></returns>
+        string GetNickname(string userToken);
+
+        void AddGame(string gameId, string player1Token, int player1TimeLimit);
+
+        void StartGame(string gameId, string player2Token, int player2TimeLimit, long startTime, string board);
+
+        void CancelGame(string gameId);
+
+        void AddWord(string gameId, string userToken, string word, int score);
+
+        void GetTime(string gameId, out int timeLimit, out long startTime);
+
+        List<string> GetWords(string gameId, string userToken);
+
+        void SetScore(string gameId, string userToken, int score);
+
+        int GetScore(string gameId, string userToken);
+
+        /// <summary>
+        /// Returns the board as a string, given the gameId
+        /// </summary>
+        /// <param name="gameId">The inquired game</param>
+        /// <returns></returns>
+        string GetBoard(string gameId);
+
+        /// <summary>
+        /// If game is pending player2Id is an empty string.
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <param name="player1Id"></param>
+        /// <param name="player2Id"></param>
+        void GetPlayers(string gameId, out string player1Id, out string player2Id);
+
+    }
+}
