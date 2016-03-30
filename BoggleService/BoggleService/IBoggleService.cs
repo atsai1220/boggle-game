7a5355621adecb1c28f8cf96baece57f5861c285
Andrew Tsai <andrewtsai1220@hotmail.com>
2016-03-30 17:13:25 -0600

    IBoggleService update interface for data contracts

--- BoggleService/BoggleService/IBoggleService.cs
+++ BoggleService/BoggleService/IBoggleService.cs
@@ -19,13 +19,13 @@ namespace Boggle
         UserTokenContract CreateUser(CreateUserBody body);
 
         [WebInvoke(Method ="POST", UriTemplate = "/games")]
-        string JoinGame(JoinGameBody body);
+        GameIdContract JoinGame(JoinGameBody body);
 
         [WebInvoke(Method = "PUT", UriTemplate = "/games")]
         void CancelJoinRequest(CancelJoinRequestBody body);
 
         [WebInvoke(Method = "PUT", UriTemplate = "/games/{gameId}")]
-        string PlayWord(PlayWordBody body, string gameId);
+        PlayWordContract PlayWord(PlayWordBody body, string gameId);
 
         [WebInvoke(Method = "GET", UriTemplate = "/games/{gameId}?Brief={brief}")]
         BoggleGameContract GameStatus(string gameId, bool brief);
